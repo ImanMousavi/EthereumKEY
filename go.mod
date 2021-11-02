fc7903a7539dad2929881e10bda000af45fbcb6b
iman <iyUCNw2yCyN9>
2021-11-02 17:02:17 +0330

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module cryptodax.app/p_wallet
+
+go 1.16
+
+require github.com/ethereum/go-ethereum v1.10.11
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+	"io/ioutil"
+)
+
+func main() {
+	keys, err := ioutil.ReadFile("test.json")
+	if err != nil {
+		panic(err)
+	}
+	password := "*********************************"
+	key, err := keystore.DecryptKey(keys, password)
+
+	privateKeyBytes := crypto.FromECDSA(key.PrivateKey)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("\naddress :\t %x", key.Address)
+	fmt.Printf("\n*** Key : \t %x", hexutil.Encode(privateKeyBytes)[2:])
+}
